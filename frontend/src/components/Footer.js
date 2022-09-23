a696d395e94518debd2e8c86da2610c2aa99b8e1
cmloof <cmlooff@gmail.com>
2022-09-23 10:09:04 -0400

    Flesh out header

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -1,7 +1,19 @@
 import React from 'react';
+import { Container, Row, Col } from 'react-bootstrap';
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer>
+      <Container>
+        <Row>
+          <Col className='text-center py-3'>
+            {/* Center the text -> Padding on the top and bottom */}
+            Copyright &copy;
+          </Col>
+        </Row>
+      </Container>
+    </footer>
+  );
 };
 
 export default Footer;
--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,9 +1,26 @@
 // rafce -> React Arrow Function Export
 
 import React from 'react';
+import { Navbar, Nav, Container } from 'react-bootstrap';
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <header>
+      <Navbar bg='dark' variant='dark' expand='lg' collapseOnSelect>
+        <Container>
+          <Navbar.Brand href='/'>Proshop</Navbar.Brand>
+          <Navbar.Toggle aria-controls='basic-navbar-nav' />
+          <Navbar.Collapse id='basic-navbar-nav'>
+            {/* ml -> Margin Start Auto -> Bootstrap Documentation */}
+            <Nav className='ms-auto'>
+              <Nav.Link href='/cart'>Cart</Nav.Link>
+              <Nav.Link href='/login'>Sign In</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </header>
+  );
 };
 
 export default Header;
